df03bfd60720786d26bcb04b84e3eb9a7f99ed53
Imam Hossain <imam.hossain@brainstation-23.com>
2022-10-02 14:51:48 +0600

    init

---
+++ lib/screen/splash.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:demo/screen/home.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  SplashScreen({Key? key}) : super(key: key);
+
+  int count = 3;
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        widget.count--;
+      });
+      if (widget.count == 0) {
+        setState(() {
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => const HomeScreen()),
+            (Route<dynamic> route) => false,
+          );
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              "Splash Screen",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              widget.count.toString(),
+              style: const TextStyle(fontSize: 20),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
